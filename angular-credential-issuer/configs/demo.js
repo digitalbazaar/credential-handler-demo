160d757438db84911ededefc06df6796f72f2d47
Matthew Collier <collier.matthew@gmail.com>
2017-08-18 21:12:51 -0400

    Update config.

---
+++ angular-credential-issuer/configs/demo.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const bedrock = require('bedrock');
+const config = bedrock.config;
+const path = require('path');
+
+config.views.vars.minify = true;
+
+// common paths
+config.paths.cache = path.join('/var', 'cache', 'credential-issuer');
+config.paths.log = path.join('/var', 'log', 'credential-issuer');
+
+// core configuration
+config.core.workers = 1;
+config.core.worker.restart = true;
+
+// master process while starting
+config.core.starting.groupId = 'adm';
+config.core.starting.userId = 'root';
+
+// master and workers after starting
+config.core.running.groupId = 'bedrock';
+config.core.running.userId = 'bedrock';
+
+// logging
+config.loggers.app.bedrock.enableChownDir = true;
+config.loggers.access.bedrock.enableChownDir = true;
+config.loggers.error.bedrock.enableChownDir = true;
--- angular-credential-issuer/lib/index.js
+++ angular-credential-issuer/lib/index.js
@@ -1,11 +1,8 @@
 /*
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
-const bedrock = require('bedrock');
 require('bedrock-views');
 require('bedrock-webpack');
 
 // load config
 require('./config');
-
-bedrock.start();
