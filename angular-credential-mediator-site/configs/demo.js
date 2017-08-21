e959e5ce4ebba412ba342be44294dc3751a26af1
Matthew Collier <collier.matthew@gmail.com>
2017-08-21 16:53:22 -0400

    Enable minification.

--- angular-credential-mediator-site/configs/demo.js
+++ angular-credential-mediator-site/configs/demo.js
@@ -22,7 +22,7 @@ config.server.domain = 'credential-mediator.demo.digitalbazaar.com';
 config.server.host = 'credential-mediator.demo.digitalbazaar.com';
 config.server.baseUri = 'https://' + config.server.host;
 
-config.views.vars.minify = false;
+config.views.vars.minify = true;
 
 // common paths
 config.paths.cache = path.join(__dirname, '..', '.cache');
