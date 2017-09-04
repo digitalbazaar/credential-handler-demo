df2a34bdd6caca784219e2db5ba982f4e082e725
Dave Longley <dlongley@digitalbazaar.com>
2017-09-04 18:00:02 -0400

    Fix credential request/store matching code.

--- angular-credential-mediator-site/components/mediator-component.js
+++ angular-credential-mediator-site/components/mediator-component.js
@@ -115,7 +115,7 @@ function Ctrl($compile, $location, $scope) {
 
     // get matching hints
     const hintOptions = await navigator.credentialMediator.ui
-      .matchCredential(operationState.credential);
+      .matchCredentialRequest(operationState.input.credentialRequestOptions);
     self.hintOptions = hintOptions.map(option => ({
       name: option.credentialHint.name,
       icon: getIconDataUrl(option.credentialHint),
@@ -145,7 +145,7 @@ function Ctrl($compile, $location, $scope) {
 
     // get matching hints
     const hintOptions = await navigator.credentialMediator.ui
-      .matchCredential(operationState.credential);
+      .matchCredential(operationState.input.credential);
     self.hintOptions = hintOptions.map(option => ({
       name: option.credentialHint.name,
       icon: getIconDataUrl(option.credentialHint),
